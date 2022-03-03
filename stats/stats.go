eae125fc5197550fbe9cb8a38b1605391472d6b5
bob34007 <bob.b@pingcap.com>
2022-03-03 16:59:00 +0800

    DumpStatic add lock

--- stats/stats.go
+++ stats/stats.go
@@ -29,53 +29,55 @@ import (
 )
 
 var (
-	Static  map[string]uint64
-	Mu sync.RWMutex
+	Static map[string]uint64
+	Mu     sync.RWMutex
 )
 
-func init(){
+func init() {
 	Static = make(map[string]uint64)
-	Static["ReadPacket"]=0
-	Static["DealPacket"]=0
-	Static["GetSQL"]=0
-	Static["DealSQL"]=0
-	Static["GetRes"]=0
-	Static["WriteRes"]=0
-	Static["PacketChanLen"]=0
-	Static["SQLChanLen"]=0
-	Static["WriteResChanLen"]=0
-	Static["ExecSQLFail"]=0
-	Static["WriteResFileFail"]=0
-	Static["FormatJsonFail"]=0
+	Static["ReadPacket"] = 0
+	Static["DealPacket"] = 0
+	Static["GetSQL"] = 0
+	Static["DealSQL"] = 0
+	Static["GetRes"] = 0
+	Static["WriteRes"] = 0
+	Static["PacketChanLen"] = 0
+	Static["SQLChanLen"] = 0
+	Static["WriteResChanLen"] = 0
+	Static["ExecSQLFail"] = 0
+	Static["WriteResFileFail"] = 0
+	Static["FormatJsonFail"] = 0
 }
 
-func AddStatic(key string , value uint64, replace bool){
+func AddStatic(key string, value uint64, replace bool) {
 	Mu.Lock()
 	defer Mu.Unlock()
-	if v,ok := Static[key]; !ok{
-		Static[key]=value
+	if v, ok := Static[key]; !ok {
+		Static[key] = value
 	} else {
 		if !replace {
 			Static[key] = v + value
-		}else {
+		} else {
 			Static[key] = value
 		}
 	}
 }
 
-func DumpStatic()  string  {
+func DumpStatic() string {
+	Mu.RLock()
+	defer Mu.RUnlock()
 	var staticStr string
-	for k,v := range Static {
-		kvStr := fmt.Sprintf("%v-%v\n",k,v)
-		staticStr+=kvStr
+	for k, v := range Static {
+		kvStr := fmt.Sprintf("%v-%v\n", k, v)
+		staticStr += kvStr
 	}
 	return staticStr
 }
 
-func GetValue (key string )uint64{
+func GetValue(key string) uint64 {
 	Mu.RLock()
 	defer Mu.RUnlock()
-	if v,ok:=Static[key];!ok{
+	if v, ok := Static[key]; !ok {
 		return 0
 	} else {
 		return v
